77b4f72a7dc0e578b86d3cad7da120e25aaf4494
patrickwang <63165862@qq.com>
2014-04-21 11:04:25 +0800

    Signed-off-by: patrickwang <63165862@qq.com>

---
+++ server/src/libbase/md5.h
@@ -0,0 +1,19 @@
+#ifndef __SAFE_LOCK_MD5_H__
+#define __SAFE_LOCK_MD5_H__
+
+#define  __alpha
+
+#ifdef __alpha
+typedef unsigned int uint32;
+#else
+//md5算法的实现里面都是这么定义的
+//如此32位机器跟64位机器来加密值会不同
+//在前面定义了__alpha
+typedef unsigned long uint32;
+#endif
+
+//只需要暴露一个接口就行了，其他看不出有什么用，声明全部放到cpp
+std::string calc_md5_string(unsigned char *input, unsigned int inlen);
+
+#endif /* !MD5_H */
+
