37516dff183d8d43c42ae977e1ac3b386da96f71
patrickwang <63165862@qq.com>
2014-04-23 10:08:58 +0800

    Signed-off-by: patrickwang <63165862@qq.com>

--- server/src/registersvr/main.cpp
+++ server/src/registersvr/main.cpp
@@ -1,8 +1,32 @@
+#include <event2/event.h>
+#include <event2/util.h>
+#include <event2/listener.h>
+#include <sys/socket.h>
+#include <glog/logging.h>
+
+void* listener_cb(struct evconnlistener* plistener, int fd, struct sockaddr * addr, int socklen, void * data);
 
 int main(int argc, char* argv[])
 {
+	evutil_socket_t fd = 0;
+	sockaddr addr;
+	int len = sizeof(sockaddr);
+	evutil_parse_sockaddr_port("127.0.0.1:5000", &addr, &len);
 
+	event_base* pbase = event_base_new();
+	evconnlistener* plistener;
+	evconnlistener_new_bind(pbase, listener_cb, NULL, LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE, 10, &addr, len);
 
+	event_base_dispatch(pbase);
+
+	evconnlistener_free(plistener);
+	event_base_free(pbase);
 
 	return 0;
 }
+
+void* listener_cb(struct evconnlistener* plistener, evutil_socket_t fd, struct sockaddr * addr, int socklen, void * data)
+{
+	LOG(INFO) << fd;
+}
+
