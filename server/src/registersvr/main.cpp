04515ebc414ad2bc969a653adb686f708c78fd89
patrickwang <63165862@qq.com>
2014-04-23 16:22:31 +0800

    Signed-off-by: patrickwang <63165862@qq.com>

--- server/src/registersvr/main.cpp
+++ server/src/registersvr/main.cpp
@@ -1,10 +1,16 @@
 #include <event2/event.h>
 #include <event2/util.h>
 #include <event2/listener.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
 #include <sys/socket.h>
 #include "include/glog_helper.h"
 
-void listener_cb(struct evconnlistener* plistener, int fd, struct sockaddr * addr, int socklen, void * data);
+void listener_cb(struct evconnlistener* plistener, evutil_socket_t fd, struct sockaddr * addr, int socklen, void * data);
+void callback_fn(evutil_socket_t fd, short eventid, void * data);
+void data_read_cb(struct bufferevent *bev, void *ctx);
+void data_write_cb(struct bufferevent *bev, void *ctx);
+void event_cb(struct bufferevent *bev, short what, void *ctx);
 
 int main(int argc, char* argv[])
 {
@@ -20,6 +26,8 @@ int main(int argc, char* argv[])
 	evconnlistener* plistener 
 		= evconnlistener_new_bind(pbase, listener_cb, reinterpret_cast<void*>(test), LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE, 10, &addr, len);
 
+	LOG(INFO) << evconnlistener_get_fd(plistener);
+
 	event_base_dispatch(pbase);
 
 	evconnlistener_free(plistener);
@@ -30,6 +38,34 @@ int main(int argc, char* argv[])
 
 void listener_cb(struct evconnlistener* plistener, evutil_socket_t fd, struct sockaddr * addr, int socklen, void * data)
 {
-	LOG(INFO) << fd << reinterpret_cast<char*>(data);
+	LOG(INFO) << fd << "|" << reinterpret_cast<char*>(data);
+	
+	struct event_base *pbase = evconnlistener_get_base(plistener);
+	struct bufferevent *bev = bufferevent_socket_new(pbase, fd, BEV_OPT_CLOSE_ON_FREE);
+	bufferevent_setcb(bev, data_read_cb, NULL, event_cb, data);
+	bufferevent_enable(bev, EV_READ | EV_WRITE);
 }
 
+void callback_fn(evutil_socket_t fd, short eventid, void * data)
+{
+	LOG(INFO) << fd << "|" << eventid;
+}
+
+void data_read_cb(struct bufferevent *bev, void *ctx)
+{
+	LOG(INFO) << reinterpret_cast<char*>(ctx);
+	struct evbuffer *input = bufferevent_get_input(bev);
+	struct evbuffer *output = bufferevent_get_output(bev);
+	evbuffer_add_buffer(output, input);
+}
+
+void data_write_cb(struct bufferevent *bev, void *ctx)
+{
+	LOG(INFO) << reinterpret_cast<char*>(ctx);
+}
+
+void event_cb(struct bufferevent *bev, short what, void *ctx)
+{
+	LOG(INFO) << reinterpret_cast<char*>(ctx) << "|" << what;
+	//BEV_EVENT_CONNECTED
+}
