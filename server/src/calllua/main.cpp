55b9e94def9a85e582c22fbcc82609697981e323
patrickwang <63165862@qq.com>
2014-05-10 01:10:00 +0800

    as
    Signed-off-by: patrickwang <63165862@qq.com>

--- server/src/calllua/main.cpp
+++ server/src/calllua/main.cpp
@@ -1,5 +1,26 @@
+#include "stdio.h"
+#include "lua.hpp"
+int tolua_tarray_open(lua_State* tolua_S);
+#include "tarray.h"
+int g_Arr[10] = { 0 };
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-	return -1;
+    lua_State * L = lua_open();
+    int i = 0;
+    for(i = 0; i < 10; i++)
+		g_Arr[i] = i;
+
+    luaopen_base(L);
+    tolua_tarray_open(L);
+    luaL_dofile(L, "../scripts/lua/array.lua");
+    printf("now in c++, re-show Arr: ");
+    for (i = 0; i < 10; i++)
+		printf("%d ", g_Arr[i]);
+
+    printf(" ");
+    lua_close(L);
+    return 0;
 }
+
+
